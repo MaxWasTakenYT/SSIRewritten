b6d5ab83c6301f196fdba24dad6966857fd1f910
JST max! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-07-25 01:33:01 +0200

    Rewrite 25/07 #1
    UwU

--- SSI-Main.cs
+++ SSI-Main.cs
@@ -5,39 +5,35 @@ class SSI
 {
     static void Main()
     {
-            // SSI - Release A (WIP) // Do we need to add Console.ReadKey();
+            // SSI - Release A (WIP)
             Console.WriteLine("SSI - SpicetifySemiautomaticInstaller");
             Console.Write("Do you want to install Spicetify? (y/n): ");
             String choiceInst = Console.ReadLine();
             Console.Write("Do you want to install Spicetify Marketplace? (y/n): ");
-            String choiceMktp = Console.ReadLine(); 
+            String choiceMktp = Console.ReadLine();
 
-        // Check user's choice, and install Spicetify
-        if (choiceInst == "y")
+        // Check user's choices, case #1
+        if (choiceInst == "y" && choiceMktp == "y")
         {
+            Console.WriteLine("Installing Spicetify with Marketplace..");
             Process.Start("powershell.exe", "-Command \"iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex\"");
-        }
-        else
-        {
-            /* For those (not autistic) who didn't understand WHY i did this,
-            i still don't know how to do like an "else: pass" thing or smth, so i
-            created a useless function to fill in the else inside this if function */
-            String uselessFuncKEKW = Console.ReadLine();
-        }
-        // Same for Spicetify Marketplace
-        if (choiceMktp == "y")
-        {
             Process.Start("powershell.exe", "-Command \"iwr -useb iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex\"");
         }
-        else if (choiceInst == "n")
+        // Case #2
+        else if (choiceInst == "y" && choiceMktp == "n")
         {
-            Console.WriteLine("Spicetify installed without Spicetify Marketplace");
-            Console.WriteLine("Exciting SSI..");
-            Environment.Exit(0);
+            Console.WriteLine("Installing Spicetify without Marketplace..");
+            Process.Start("powershell.exe", "-Command \"iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex\"");
         }
-        else
+        // Case #3
+        else if (choiceInst == "n" && choiceMktp == "y")
         {
-            Console.WriteLine("You just started a Spicetify installer, and selected not to install Spicetify(?)");
+            Console.WriteLine("Installing Marketplace without Spicetify.. (lol)");
+            Process.Start("powershell.exe", "-Command \"iwr -useb iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex\"");
+        }
+        else if (choiceInst == "n" && choiceMktp == "n")
+        {
+            Console.WriteLine("Don't wanna install anything? Ok, ok, your business..");
             Environment.Exit(0);
         }
     }
