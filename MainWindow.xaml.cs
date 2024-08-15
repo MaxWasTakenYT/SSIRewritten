7ff93b00922c94dfb216475be749fca8bc16a223
JST max! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-08-15 07:26:55 +0000

    Uploaded TU2 project files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,23 +30,11 @@ namespace SSIRewritten
             if (json.action == "install")
             {
                 bool installSpicetify = json.installSpicetify;
-                bool installMarketplace = json.installMarketplace;
 
-                if (installSpicetify && installMarketplace)
+                if (installSpicetify)
                 {
-                    MessageBox.Show("installing Spicetify and Marketplace.. -~-*(# 3#)/*-~-");
+                    MessageBox.Show("the script will ask you to install the marketplace!.. -~-*(# 3#)/*-~-");
                     Process.Start(new ProcessStartInfo("powershell", "-Command \"iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex\"")).StandardOutput.ReadToEnd();
-                    Process.Start(new ProcessStartInfo("powershell", "-Command \"iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex\"")).StandardOutput.ReadToEnd();
-                }
-                else if (installSpicetify)
-                {
-                    MessageBox.Show("installing Spicetify.. -~-*(SwS)/*-~-");
-                    Process.Start(new ProcessStartInfo("powershell", "-Command \"iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex\"")).StandardOutput.ReadToEnd();
-                }
-                else if (installMarketplace)
-                {
-                    MessageBox.Show("installing Marketplace.. -~-*(MwM)/*-~-");
-                    Process.Start(new ProcessStartInfo("powershell", "-Command \"iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex\"")).StandardOutput.ReadToEnd();
                 }
                 else
                 {
